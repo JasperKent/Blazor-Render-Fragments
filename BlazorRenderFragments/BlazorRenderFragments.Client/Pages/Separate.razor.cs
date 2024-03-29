4d42a371f4ec912251b8142a1f32466a522614c3
Jasper Kent <software@jasperkent.com>
2024-03-29 08:49:56 +0000

    Add project files.

---
+++ BlazorRenderFragments/BlazorRenderFragments.Client/Pages/Separate.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorRenderFragments.Client.Pages;
+
+public partial class Separate
+{
+    private string _circle = """
+                <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+                    <circle r="45" cx="50" cy="50" fill="red" />
+                </svg>
+                """;
+
+}
